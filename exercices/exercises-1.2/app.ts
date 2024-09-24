4807363984049820fbc9762eece0e512f1f8ba24
Helalouch Aymane <aymane.helalouch@student.vinci.be>
2024-09-24 11:57:48 +0200

    exo 1-3

--- exercices/exercises-1.2/app.ts
+++ exercices/exercises-1.2/app.ts
@@ -5,6 +5,13 @@ import pizzaRouter from "./routes/pizzas";
 import filmsRouter from "./routes/films";
 
 const app = express();
+let numGet = 0;
+
+app.use ((req,_res,next)=>{
+    if (req.method === "GET")
+    console.log("GET counter : ", numGet++ );
+    next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -12,5 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/users", usersRouter);
 app.use("/pizzas", pizzaRouter);
 app.use("/films", filmsRouter);
+
+
 
 export default app;
--- exercices/exercises-1.2/routes/films.ts
+++ exercices/exercises-1.2/routes/films.ts
@@ -1,40 +1,37 @@
 import { Router } from "express";
 import { Film } from "../types";
 
-
 const router = Router();
 
-const defaultFilms : Film [] = [
+const defaultFilms: Film[] = [
   {
-  id: 1,
-  title: "Retour vers le futur",
-  director: "Aymane",
-  duration : 300,
-},
-{
-  id: 2,
-  title: "Seigneur des anneaux",
-  director: "test",
-  duration: 350,
-},
-{
-  id: 3,
-  title: "Harry Potter",
-  director: "Mozarella, Tomates, Oignons, Poivrons, Champignons, Olives",
-  duration:430,
-},
-{
-  id: 4,
-  title: "Pokemon",
-  director: "Sacha",
-  duration : 230,
-},
-]
+    id: 1,
+    title: "Retour vers le futur",
+    director: "Aymane",
+    duration: 300,
+  },
+  {
+    id: 2,
+    title: "Seigneur des anneaux",
+    director: "test",
+    duration: 350,
+  },
+  {
+    id: 3,
+    title: "Harry Potter",
+    director: "Mozarella, Tomates, Oignons, Poivrons, Champignons, Olives",
+    duration: 430,
+  },
+  {
+    id: 4,
+    title: "Pokemon",
+    director: "Sacha",
+    duration: 230,
+  },
+];
 
-router.get("/", (_req,res)=>{
+router.get("/", (_req, res) => {
   return res.json(defaultFilms);
-
 });
-
 
 export default router;
